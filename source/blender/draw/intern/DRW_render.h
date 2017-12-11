6687dd01789130cf5b7d12df5898696b610c4b61
Ulysse Martin <youle31@users.noreply.github.com>
2017-12-11 22:00:47 +0100

    remove unused draw_manager API

--- source/blender/draw/intern/DRW_render.h
+++ source/blender/draw/intern/DRW_render.h
@@ -457,7 +457,6 @@ const DRWContextState *DRW_context_state_get(void);
 
 /*****************************GAME ENGINE***********************************/
 void *DRW_viewport_engine_data_ensure(void *engine_type);
-void DRW_game_bind_shgroup_shader(DRWShadingGroup *shgroup);
 DRWShadingGroup *DRW_game_shgroups_from_pass_get(DRWPass *pass);
 DRWShadingGroup *DRW_game_shgroup_next(DRWShadingGroup *current);
 void DRW_game_call_update_obmat(DRWShadingGroup *shgroup, struct Gwn_Batch *batch, float obmat[4][4]);
