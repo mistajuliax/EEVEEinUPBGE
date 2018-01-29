fa23434df0e73da30189dd2561bbb5638ceb533e
Ulysse Martin <youle31@users.noreply.github.com>
2018-01-29 17:53:46 +0100

    increase Max_texture_units
    I set the max to 32. In most cases this should be enough, else we'll fix
    that in another way (fixes crashes at wanderer scene exit)

--- source/gameengine/Rasterizer/RAS_Texture.h
+++ source/gameengine/Rasterizer/RAS_Texture.h
@@ -59,7 +59,7 @@ public:
 	/// Return all the OpenGL cube map face target, e.g GL_TEXTURE_CUBE_MAP_POSITIVE_Z.
 	static const std::array<int, 6>& GetCubeMapTargets();
 
-	enum {MaxUnits = 8};
+	enum {MaxUnits = 32}; // EEVEE INTEGRATION: Warning: can be increased again if we have crashes
 
 	virtual void CheckValidTexture() = 0;
 	virtual void ActivateTexture(int unit) = 0;
